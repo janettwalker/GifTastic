b57669c4615b4f7f833c904488f775d5f8c71ae8
Janet Walker <janetwalker@Janets-MBP.attlocal.net>
2017-03-29 09:59:09 -0400

    managed to get the gifs to appear but still working on getting the gif image to correspond to the button text

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,8 @@
+//array of animals that will display when page opens
 var randomAnimal = ["koala", "rabbit", "platypus", "opposum"];
 console.log(randomAnimal);
-//call function when button is clicked
-//make it so that the button searches for the correct data
-// $('button').on('click', function(){
-// 	var x = $(this).data("search");
 
-// 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + randomAnimal + "&api_key=dc6zaTOxFJmzC&limit=10";
-// 	console.log(queryURL)
-// })
-
+//function that renders the buttons from the array of randomAnimals
 function showButtons() {
 
 	//clear out text from button div
@@ -24,8 +18,8 @@ function showButtons() {
 		y.addClass("animal");
 
 		//add data-attribute
-		y.attr("data-name"),
-			randomAnimal[i];
+		y.attr("data-name", randomAnimal[i]);
+			
 
 		//give button text
 		y.text(randomAnimal[i]);
@@ -61,39 +55,58 @@ $("#addButton").on("click", function(event){
 //setup calls to api
 
 
-
+//function that will call the gif api and retrieve the data based on the button input
 function showAnimalGif() {
+
+	//run function on #button click
 	$("#button").on("click", function(){
+
+
 	var animalData = $(this).attr("data-name");
+
+	//queryURL to find the giphy api
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         animalData + "&api_key=dc6zaTOxFJmzC&limit=10";
 	console.log(queryURL)
 
+	//function that retrieves api data (the promise)
 	$.ajax({
 		url: queryURL,
 		method: "GET"
 	})
 
+	//function that determines what happens to the retrieved data
 	.done(function(response) {
 	
+	//variable for the data that we get from response
 	var results = response.data;
 	console.log(results)
 
-	for (var i = 0; i < response.data; i++) {
+	//create a for loop to loop through the data from the response
+	for (var i = 0; i < results.length; i++) {
 
+	//create a div for the rating and gif images
 	var gifDiv = $("<div class='item'>");
-
+ 	
+ 	//variable for the rating in array of results
 	var rating = results[i].rating;
 
+	//create html paragraph for the rating text
 	var p = $("<p>").text("Rating: " + rating);
 
+	//create html img for the gifs
 	var gifImage = $("<img>");
 
-	gifImage.attr("src", rsults[i].images.fixed_height.url);
+	//assigning an attribute to the image and giving it a source that is from the array of results
+	gifImage.attr("src", results[i].images.fixed_height.url);
 
+	//prepend rating text to the gifDiv
 	gifDiv.prepend(p);
+
+	//prepend the gif images to the gifDiv
 	gifDiv.prepend(gifImage);
 
+	//adding the gifDiv to the id="gifHere"
 	$("#gifHere").append(gifDiv);
 
 	};
@@ -104,8 +117,11 @@ function showAnimalGif() {
 
 };
 
-
+//call the showButtons function
 showButtons();
 
+//call the showAnimalGif function
 showAnimalGif ();
+
+
 
